8bad865f7961c4acd95029c5a12916c8b81f3a42
coco-bl <blbooker1996@gmail.com>
2025-05-10 15:02:32 -0400

    Fix indentation for storage account name reference in storage container resource

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -10,6 +10,6 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = var.container_name
-   storage_account_name  = azurerm_storage_account.storage_account.name
+  storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = var.container_access_type
 }
